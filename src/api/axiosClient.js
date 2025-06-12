041247f51cfc22a911f6efc2dc1d13e91f8a8bb5
eslamawd <eeslamawood@gmail.com>
2025-06-13 02:00:25 +0300

    up protected Route commit

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default function api() {
     const api = axios.create({
-        baseURL: 'https://wholesale-app-backend-laravel-production.up.railway.app/',
+        baseURL: 'https://wholesale-app-backend-laravel-production.up.railway.app',
         withCredentials: true
     })
    axios.defaults.withCredentials = true;
