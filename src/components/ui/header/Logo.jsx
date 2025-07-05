bc17916e68b6f7783d96722de427a4b94455f591
eslamawd <eeslamawood@gmail.com>
2025-07-05 21:10:16 +0300

    run react update  Logo

--- src/components/ui/header/Logo.jsx
+++ src/components/ui/header/Logo.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { Store } from 'lucide-react';
-import viteLogo from '../../../../public/vite.jpg'; 
 
 
 function Logo() {
@@ -11,7 +10,7 @@ function Logo() {
       className="flex items-center transition-opacity hover:opacity-80"
     >
       <div className="flex items-center">
-        <img src={viteLogo} alt="ServexLB Logo" className="w-16 h-16 mr-2 rounded-full" /> 
+        <img src='/vite.jpg' alt="ServexLB Logo" className="w-16 h-16 mr-2 rounded-full" /> 
         <span className="font-bold text-xl">ServexLB</span>
       </div>
     </Link>
